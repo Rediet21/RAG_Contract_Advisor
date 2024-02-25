c545de1fb0461b7e7cd4c8c1ab292d20a936561d
Rediet21 <redogirma@gmail.com>
2024-02-26 01:50:42 +0300

    code utilization

--- src/front.py
+++ src/front.py
@@ -13,43 +13,44 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.question_answering import load_qa_chain
 from template import css, bot_template, user_template
+from utils.utils import get_pdf_text, get_text_chunks, get_embedding, get_conversation
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+# def get_pdf_text(pdf_docs):
+#     text = ""
+#     for pdf in pdf_docs:
+#         pdf_reader = PdfReader(pdf)
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
+#     return text
 
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size = 500,
-        chunk_overlap = 100,
-        length_function = len
-        )
-    chunks = text_splitter.split_text(text)
-    return chunks
+# def get_text_chunks(text):
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size = 500,
+#         chunk_overlap = 100,
+#         length_function = len
+#         )
+#     chunks = text_splitter.split_text(text)
+#     return chunks
 
-def get_embedding(text_chunks):
-     #persist_directory = 'db'
-     embeddings = OpenAIEmbeddings()
+# def get_embedding(text_chunks):
+#      #persist_directory = 'db'
+#      embeddings = OpenAIEmbeddings()
      
-     vectordb = FAISS.from_texts(texts = text_chunks,
-                                  embedding=embeddings,
-                                  )
-     return vectordb
+#      vectordb = FAISS.from_texts(texts = text_chunks,
+#                                   embedding=embeddings,
+#                                   )
+#      return vectordb
 
-def get_conversation(vectordb):
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectordb.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
+# def get_conversation(vectordb):
+#     llm = ChatOpenAI()
+#     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages=True)
+#     conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         retriever=vectordb.as_retriever(),
+#         memory=memory
+#     )
+#     return conversation_chain
 
 
 def handle_input(query, conversation):
